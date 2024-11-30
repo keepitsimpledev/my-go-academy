4d9fa777be55df1726c645c494356860ef92a13f
dev <keep.it.simple.dev@gmail.com>
2024-11-30 15:52:15 -0500

    learn go with tests - select (#18)

---
+++ learn-go-with-tests/11-select/racer.go
@@ -0,0 +1,40 @@
+package racer
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		//nolint:gosec
+		response, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+
+		response.Body.Close()
+		close(ch)
+	}()
+
+	return ch
+}
