0a7756638738b264f3dfe092ec4236eca82bbada
dev <keep.it.simple.dev@gmail.com>
2024-11-30 15:52:16 -0500

    todo app - 1.14 (cont'd) (#41)

--- to-do-app/1.14/race.go
+++ to-do-app/1.14/race.go
@@ -10,24 +10,25 @@ const count = 1000
 const parity = 2
 
 type AlertingValue struct {
-	writer io.Writer
-	value  int
+	writer            io.Writer
+	value, numUpdates int
 }
 
-func (a AlertingValue) updateAndAlert(newValue int) {
-	a.value = newValue
-	fmt.Fprintln(a.writer, a.value)
+func (a *AlertingValue) updateAndAlert(addAmount int) {
+	a.value += addAmount
+	a.numUpdates++
+	fmt.Fprintf(a.writer, "value: %d. numUpdates: %d\n", a.value, a.numUpdates)
 }
 
 func UpdateNumber(writer io.Writer) {
-	alertingValue := AlertingValue{writer: writer, value: 0}
+	alertingValue := AlertingValue{writer: writer, value: 0, numUpdates: 0}
 
 	var waitGroup sync.WaitGroup
 
 	go func(av *AlertingValue, wg *sync.WaitGroup) {
 		for i := 0; i < count; i++ {
-			if i%2 == 0 {
-				av.updateAndAlert(i)
+			if i%parity == 0 {
+				av.updateAndAlert(parity)
 			}
 		}
 		wg.Done()
@@ -35,8 +36,8 @@ func UpdateNumber(writer io.Writer) {
 
 	go func(av *AlertingValue, wg *sync.WaitGroup) {
 		for i := 0; i < count; i++ {
-			if i%2 == 1 {
-				av.updateAndAlert(i)
+			if i%parity == 1 {
+				av.updateAndAlert(i % parity)
 			}
 		}
 		wg.Done()
